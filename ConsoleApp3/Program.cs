16f3efc2f471a49b5e0fbcce4cc9a06aa14970d0
aarujarv <a.arujarv@gmail.com>
2023-01-08 19:00:53 +0200

    new

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter a number");
+int userNumber = Int32.Parse(Console.ReadLine());
+for (int i = 0; i < 10; i++)
+{
+
+    Console.WriteLine($"{i + 1} * {userNumber} = {(i + 1) * userNumber}");
+
+
+}
